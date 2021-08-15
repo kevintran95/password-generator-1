124b1084ebf5527e6898cbe71b2e493dd2ddb6d0
Kevin Tran <kevin_tran95@yahoo.com>
2021-08-14 19:16:25 -0700

    added the char set that will be used

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var special = ["!", "#", "$", "%", "&", "‘", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=",">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"]
+var numerical = [0,1,2,3,4,5,6,7,8,9];
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var charString = ["uppercase", "lowercase", "numerical", "special"];
+
+
+var password = "";
+var userInput = "";
+
 
 // Write password to the #password input
 function writePassword() {
